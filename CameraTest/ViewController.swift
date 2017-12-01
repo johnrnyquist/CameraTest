0cc5bf7776bf5623114c8ffdb68092c13118c5df
johnrnyquist <john@nyquist.net>
2017-12-01 06:33:58 -0700

    Test on taking and saving a picture to the Photo library.

--- CameraTest/ViewController.swift
+++ CameraTest/ViewController.swift
@@ -1,25 +1,50 @@
-//
-//  ViewController.swift
-//  CameraTest
-//
-//  Created by John Nyquist on 11/15/17.
-//  Copyright © 2017 Nyquist Art + Logic LLC. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:
+        UIViewController,
+        UINavigationControllerDelegate,
+        UIImagePickerControllerDelegate  {
+    
+    @IBOutlet weak var imageTake: UIImageView!
+    
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: - Take image
+    @IBAction func takePhoto(_ sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
     }
-
-
+    
+    //MARK: - Saving Image here
+    @IBAction func save(_ sender: AnyObject) {
+        if let image = imageTake.image {
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+    }
+    
+    //MARK: - Add image to Library
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    //MARK: - Done image capture here
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicker.dismiss(animated: true, completion: nil)
+        imageTake.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
 }
-
